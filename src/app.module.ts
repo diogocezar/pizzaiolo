8e68fdac26591f4d1e585ea728cc3658f67aa3c3
Diogo Cezar <8019111+diogocezar@users.noreply.github.com>
2022-10-22 10:25:06 -0300

    feat: includding slack integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { SlackModule } from 'nestjs-slack';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    SlackModule.forRoot({
+      type: 'webhook',
+      channels: [
+        {
+          name: process.env.CHANNEL,
+          url: process.env.WEBHOOK_URL,
+        },
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { SlackService } from 'nestjs-slack';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private slack: SlackService) {}
+  sendMessage(): string {
+    const message = 'Pizza is ready!';
+    console.log(process.env);
+    this.slack.sendText(message, { channel: process.env.CHANNEL });
+    return message;
   }
 }
