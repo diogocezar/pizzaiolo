77126ba3e7b458a4afeda11fde88475f5fdf6c8d
joao208 <augustoj311@gmail.com>
2022-10-23 17:24:55 -0300

    feat: some refacts

--- src/helper.ts
+++ src/helper.ts
@@ -1,63 +1,109 @@
 export const validatePayload = (payload: any) => {
-  const { action } = payload;
+  const { action } = payload
+
   const validActions = [
-    `opened`,
-    `closed`,
-    `reopened`,
-    `submitted`,
-    `created`,
-    `resolved`,
-    `unresolved`,
-  ];
-  if (!validActions.includes(action)) {
-    return false;
+    'opened',
+    'closed',
+    'reopened',
+    'submitted',
+    'created',
+    'resolved',
+    'unresolved',
+  ]
+
+  return validActions.includes(action)
+}
+
+const openedPullRequest = () => {
+  return `🍕 Saindo uma Pizza do Forno! 🍕\n`
+}
+
+const closedPullRequest = ({ merged }: { merged: boolean }) => {
+  if (merged) {
+    return `🔒 Uma PizzaRequest fechada com sucesso!\n`
   }
-  return true;
-};
+
+  return `🔒 Uma PizzaRequest fechada com commits não mesclados!\n`
+}
+
+// TODO remove this any
+const submittedPullRequest = ({ review }: { review: any }) => {
+  let message = ''
+
+  if (review) {
+    message += `✅ Uma PizzaRequest foi revisada com sucesso!\n`
+    message += `Usuário que revisou: ${review.user.login}\n`
+  }
+
+  return message
+}
+
+const createdPullRequest = ({ comment }: { comment: any }) => {
+  let message = ''
+
+  if (comment) {
+    message += `💭 Um comentário foi adicionado a uma PizzaRequest!\n`
+    message += `Comentário: ${comment.body}\n`
+    message += `Usuário que comentou: ${comment.user.login}\n`
+  }
+
+  return message
+}
+
+const resolvedPUllRequest = ({ thread }: { thread: any }) => {
+  let message = ''
+
+  message += `✅ Uma thread foi resolvida!\n`
+
+  if (!thread) return message
+
+  message += `URL: ${thread.comments[0].url}\n`
+
+  return message
+}
+
+const unresolvedPullRequest = ({ thread }: { thread: any }) => {
+  let message = ''
+
+  message += `👉 Uma thread foi marcada como aberta!\n`
+
+  if (!thread) return message
+
+  message += `URL: ${thread.comments[0].url}\n`
+
+  return message
+}
 
 export const getMessage = (payload: any) => {
-  const { action, number, pull_request, review, comment, thread } = payload;
-  let message = ``;
-  if (number) message += `Número do Pedido: ${number}\n`;
-  if (pull_request) {
-    const { user, url, created_at, merged } = pull_request;
-    if (action === `opened`) {
-      message += `🍕 Saindo uma Pizza do Forno! 🍕\n`;
-    }
-    if (action === `closed` && merged === false) {
-      message += `🔒 Uma PizzaRequest fechada com commits não mesclados!\n`;
-    }
-    if (action === `closed` && merged === true) {
-      message += `🔒 Uma PizzaRequest fechada com sucesso!\n`;
-    }
-    if (action === `submitted`) {
-      if (review) {
-        message += `✅ Uma PizzaRequest foi revisada com sucesso!\n`;
-        message += `Usuário que revisou: ${review.user.login}\n`;
-      }
-    }
-    if (action === `created`) {
-      if (comment) {
-        message += `💭 Um comentário foi adicionado a uma PizzaRequest!\n`;
-        message += `Comentário: ${comment.body}\n`;
-        message += `Usuário que comentou: ${comment.user.login}\n`;
-      }
-    }
-    if (action === `resolved`) {
-      message += `✅ Uma thread foi resolvida!\n`;
-      if (thread) {
-        message += `URL: ${thread.comments[0].url}\n`;
-      }
-    }
-    if (action === `unresolved`) {
-      message += `👉 Uma thread foi marcada como aberta!\n`;
-      if (thread) {
-        message += `URL: ${thread.comments[0].url}\n`;
-      }
-    }
-    message += `Data do Pedido: ${created_at}\n`;
-    message += `Usuário: ${user.login}\n`;
-    message += `URL: ${url}`;
+  const { action, number, pull_request, review, comment, thread } = payload
+
+  let message = ``
+
+  if (number) message += `Número do Pedido: ${number}\n`
+
+  if (!pull_request) return message
+
+  const availableActions = {
+    opened: openedPullRequest,
+    closed: closedPullRequest,
+    submitted: submittedPullRequest,
+    created: createdPullRequest,
+    resolved: resolvedPUllRequest,
+    unresolved: unresolvedPullRequest,
   }
-  return message;
-};
+
+  const { user, url, created_at, merged } = pull_request
+
+  message += availableActions[action]({
+    merged,
+    review,
+    comment,
+    thread,
+  })
+
+  message += `Data do Pedido: ${created_at}\n`
+  message += `Usuário: ${user.login}\n`
+  message += `URL: ${url}`
+
+  return message
+}
