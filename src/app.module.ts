2c1e6fae6943f0d214afb073316cb6dd079e1d9e
joao208 <augustoj311@gmail.com>
2022-10-23 17:25:10 -0300

    feat: lint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { SlackModule } from 'nestjs-slack';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config'
+import { SlackModule } from 'nestjs-slack'
 
 @Module({
   imports: [
