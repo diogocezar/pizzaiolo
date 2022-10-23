cafd314e1cb6877c032cc5e8b7cf496a68d86b2c
joao208 <augustoj311@gmail.com>
2022-10-23 17:25:03 -0300

    feat: some refacts

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,19 @@
-import { Injectable } from '@nestjs/common';
-import { SlackService } from 'nestjs-slack';
-import { validatePayload, getMessage } from './helper';
+import { Injectable } from '@nestjs/common'
+import { SlackService } from 'nestjs-slack'
+import { validatePayload, getMessage } from './helper'
 
 @Injectable()
 export class AppService {
   constructor(private slack: SlackService) {}
   sendMessage(payload): boolean {
-    if (!validatePayload(payload)) return false;
-    const message = getMessage(payload);
-    console.log(message);
-    this.slack.sendText(message, { channel: process.env.CHANNEL });
-    return true;
+    if (!validatePayload(payload)) return false
+
+    const message = getMessage(payload)
+
+    console.log(message)
+
+    this.slack.sendText(message, { channel: process.env.CHANNEL })
+
+    return true
   }
 }
