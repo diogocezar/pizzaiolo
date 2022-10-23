8b2eaa350967e60407b39b5ef7782f7dd27a00ef
Diogo Cezar <8019111+diogocezar@users.noreply.github.com>
2022-10-23 20:12:10 -0300

    refact: refacting in services

---
+++ src/pizzaiolo/pizzaiolo.service.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PizzaioloService } from './pizzaiolo.service';
+
+describe('PizzaioloService', () => {
+  let service: PizzaioloService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [PizzaioloService],
+    }).compile();
+
+    service = module.get<PizzaioloService>(PizzaioloService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
