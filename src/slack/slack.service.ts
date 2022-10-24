0ebd216d03ab8a5ca23775966ed73184a89e46a4
joao208 <augustoj311@gmail.com>
2022-10-24 00:31:27 -0300

    feat: using slack api instead sdk

--- src/slack/slack.service.ts
+++ src/slack/slack.service.ts
@@ -1,10 +1,45 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { Injectable } from '@nestjs/common'
-import { SlackService as LibSlackService } from 'nestjs-slack'
+import * as axios from 'axios'
 
 @Injectable()
 export class SlackService {
-  constructor(private slackService: LibSlackService) {}
-  sendMessage(message: string): void {
-    this.slackService.sendText(message, { channel: process.env.CHANNEL })
+  api: string
+  client: axios.Axios
+
+  constructor() {
+    this.api = 'https://slack.com/api'
+    this.client = this.instanceClient()
+  }
+
+  instanceClient() {
+    // @ts-ignore
+    return axios.create({
+      baseURL: this.api,
+      timeout: 1000,
+      headers: {
+        Authorization: `Bearer ${process.env.SLACK_TOKEN}`,
+      },
+    })
+  }
+
+  async sendMessage(message: string, thread_ts?: string): Promise<any> {
+    const { data } = await this.client.post('/chat.postMessage', {
+      channel: process.env.SLACK_CHANNEL,
+      text: message,
+      ...(thread_ts && { thread_ts }),
+    })
+
+    return data
+  }
+
+  async addReaction(reaction: string, timestamp: string): Promise<void> {
+    if (!timestamp) return
+
+    await this.client.post('/reactions.add', {
+      channel: process.env.SLACK_CHANNEL,
+      name: reaction,
+      timestamp,
+    })
   }
 }
