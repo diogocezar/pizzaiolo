b65494366043d5ad695398d60414fc0c9b949578
joao208 <augustoj311@gmail.com>
2022-10-23 22:30:56 -0300

    fix: lint

--- src/types/review.d.ts
+++ src/types/review.d.ts
@@ -1,297 +1,297 @@
 export interface User {
-  login: string;
-  id: number;
-  node_id: string;
-  avatar_url: string;
-  gravatar_id: string;
-  url: string;
-  html_url: string;
-  followers_url: string;
-  following_url: string;
-  gists_url: string;
-  starred_url: string;
-  subscriptions_url: string;
-  organizations_url: string;
-  repos_url: string;
-  events_url: string;
-  received_events_url: string;
-  type: string;
-  site_admin: boolean;
+  login: string
+  id: number
+  node_id: string
+  avatar_url: string
+  gravatar_id: string
+  url: string
+  html_url: string
+  followers_url: string
+  following_url: string
+  gists_url: string
+  starred_url: string
+  subscriptions_url: string
+  organizations_url: string
+  repos_url: string
+  events_url: string
+  received_events_url: string
+  type: string
+  site_admin: boolean
 }
 
 export interface Html {
-  href: string;
+  href: string
 }
 
 export interface PullRequest {
-  href: string;
+  href: string
 }
 
 export interface Links {
-  html: Html;
-  pull_request: PullRequest;
+  html: Html
+  pull_request: PullRequest
 }
 
 export interface Review {
-  id: number;
-  node_id: string;
-  user: User;
-  body?: any;
-  commit_id: string;
-  submitted_at: Date;
-  state: string;
-  html_url: string;
-  pull_request_url: string;
-  author_association: string;
-  _links: Links;
+  id: number
+  node_id: string
+  user: User
+  body?: any
+  commit_id: string
+  submitted_at: Date
+  state: string
+  html_url: string
+  pull_request_url: string
+  author_association: string
+  _links: Links
 }
 
 export interface Owner {
-  login: string;
-  id: number;
-  node_id: string;
-  avatar_url: string;
-  gravatar_id: string;
-  url: string;
-  html_url: string;
-  followers_url: string;
-  following_url: string;
-  gists_url: string;
-  starred_url: string;
-  subscriptions_url: string;
-  organizations_url: string;
-  repos_url: string;
-  events_url: string;
-  received_events_url: string;
-  type: string;
-  site_admin: boolean;
+  login: string
+  id: number
+  node_id: string
+  avatar_url: string
+  gravatar_id: string
+  url: string
+  html_url: string
+  followers_url: string
+  following_url: string
+  gists_url: string
+  starred_url: string
+  subscriptions_url: string
+  organizations_url: string
+  repos_url: string
+  events_url: string
+  received_events_url: string
+  type: string
+  site_admin: boolean
 }
 
 export interface Repo {
-  id: number;
-  node_id: string;
-  name: string;
-  full_name: string;
-  private: boolean;
-  owner: Owner;
-  html_url: string;
-  description?: any;
-  fork: boolean;
-  url: string;
-  forks_url: string;
-  keys_url: string;
-  collaborators_url: string;
-  teams_url: string;
-  hooks_url: string;
-  issue_events_url: string;
-  events_url: string;
-  assignees_url: string;
-  branches_url: string;
-  tags_url: string;
-  blobs_url: string;
-  git_tags_url: string;
-  git_refs_url: string;
-  trees_url: string;
-  statuses_url: string;
-  languages_url: string;
-  stargazers_url: string;
-  contributors_url: string;
-  subscribers_url: string;
-  subscription_url: string;
-  commits_url: string;
-  git_commits_url: string;
-  comments_url: string;
-  issue_comment_url: string;
-  contents_url: string;
-  compare_url: string;
-  merges_url: string;
-  archive_url: string;
-  downloads_url: string;
-  issues_url: string;
-  pulls_url: string;
-  milestones_url: string;
-  notifications_url: string;
-  labels_url: string;
-  releases_url: string;
-  deployments_url: string;
-  created_at: Date;
-  updated_at: Date;
-  pushed_at: Date;
-  git_url: string;
-  ssh_url: string;
-  clone_url: string;
-  svn_url: string;
-  homepage?: any;
-  size: number;
-  stargazers_count: number;
-  watchers_count: number;
-  language: string;
-  has_issues: boolean;
-  has_projects: boolean;
-  has_downloads: boolean;
-  has_wiki: boolean;
-  has_pages: boolean;
-  forks_count: number;
-  mirror_url?: any;
-  archived: boolean;
-  disabled: boolean;
-  open_issues_count: number;
-  license?: any;
-  forks: number;
-  open_issues: number;
-  watchers: number;
-  default_branch: string;
-  allow_squash_merge: boolean;
-  allow_merge_commit: boolean;
-  allow_rebase_merge: boolean;
-  delete_branch_on_merge: boolean;
+  id: number
+  node_id: string
+  name: string
+  full_name: string
+  private: boolean
+  owner: Owner
+  html_url: string
+  description?: any
+  fork: boolean
+  url: string
+  forks_url: string
+  keys_url: string
+  collaborators_url: string
+  teams_url: string
+  hooks_url: string
+  issue_events_url: string
+  events_url: string
+  assignees_url: string
+  branches_url: string
+  tags_url: string
+  blobs_url: string
+  git_tags_url: string
+  git_refs_url: string
+  trees_url: string
+  statuses_url: string
+  languages_url: string
+  stargazers_url: string
+  contributors_url: string
+  subscribers_url: string
+  subscription_url: string
+  commits_url: string
+  git_commits_url: string
+  comments_url: string
+  issue_comment_url: string
+  contents_url: string
+  compare_url: string
+  merges_url: string
+  archive_url: string
+  downloads_url: string
+  issues_url: string
+  pulls_url: string
+  milestones_url: string
+  notifications_url: string
+  labels_url: string
+  releases_url: string
+  deployments_url: string
+  created_at: Date
+  updated_at: Date
+  pushed_at: Date
+  git_url: string
+  ssh_url: string
+  clone_url: string
+  svn_url: string
+  homepage?: any
+  size: number
+  stargazers_count: number
+  watchers_count: number
+  language: string
+  has_issues: boolean
+  has_projects: boolean
+  has_downloads: boolean
+  has_wiki: boolean
+  has_pages: boolean
+  forks_count: number
+  mirror_url?: any
+  archived: boolean
+  disabled: boolean
+  open_issues_count: number
+  license?: any
+  forks: number
+  open_issues: number
+  watchers: number
+  default_branch: string
+  allow_squash_merge: boolean
+  allow_merge_commit: boolean
+  allow_rebase_merge: boolean
+  delete_branch_on_merge: boolean
 }
 
 export interface Head {
-  label: string;
-  ref: string;
-  sha: string;
-  user: User;
-  repo: Repo;
+  label: string
+  ref: string
+  sha: string
+  user: User
+  repo: Repo
 }
 
 export interface Base {
-  label: string;
-  ref: string;
-  sha: string;
-  user: User;
-  repo: Repo;
+  label: string
+  ref: string
+  sha: string
+  user: User
+  repo: Repo
 }
 
 export interface Self {
-  href: string;
+  href: string
 }
 
 export interface Issue {
-  href: string;
+  href: string
 }
 
 export interface Comments {
-  href: string;
+  href: string
 }
 
 export interface ReviewComments {
-  href: string;
+  href: string
 }
 
 export interface ReviewComment {
-  href: string;
+  href: string
 }
 
 export interface Commits {
-  href: string;
+  href: string
 }
 
 export interface Statuses {
-  href: string;
+  href: string
 }
 
 export interface Repository {
-  id: number;
-  node_id: string;
-  name: string;
-  full_name: string;
-  private: boolean;
-  owner: Owner;
-  html_url: string;
-  description?: any;
-  fork: boolean;
-  url: string;
-  forks_url: string;
-  keys_url: string;
-  collaborators_url: string;
-  teams_url: string;
-  hooks_url: string;
-  issue_events_url: string;
-  events_url: string;
-  assignees_url: string;
-  branches_url: string;
-  tags_url: string;
-  blobs_url: string;
-  git_tags_url: string;
-  git_refs_url: string;
-  trees_url: string;
-  statuses_url: string;
-  languages_url: string;
-  stargazers_url: string;
-  contributors_url: string;
-  subscribers_url: string;
-  subscription_url: string;
-  commits_url: string;
-  git_commits_url: string;
-  comments_url: string;
-  issue_comment_url: string;
-  contents_url: string;
-  compare_url: string;
-  merges_url: string;
-  archive_url: string;
-  downloads_url: string;
-  issues_url: string;
-  pulls_url: string;
-  milestones_url: string;
-  notifications_url: string;
-  labels_url: string;
-  releases_url: string;
-  deployments_url: string;
-  created_at: Date;
-  updated_at: Date;
-  pushed_at: Date;
-  git_url: string;
-  ssh_url: string;
-  clone_url: string;
-  svn_url: string;
-  homepage?: any;
-  size: number;
-  stargazers_count: number;
-  watchers_count: number;
-  language: string;
-  has_issues: boolean;
-  has_projects: boolean;
-  has_downloads: boolean;
-  has_wiki: boolean;
-  has_pages: boolean;
-  forks_count: number;
-  mirror_url?: any;
-  archived: boolean;
-  disabled: boolean;
-  open_issues_count: number;
-  license?: any;
-  forks: number;
-  open_issues: number;
-  watchers: number;
-  default_branch: string;
+  id: number
+  node_id: string
+  name: string
+  full_name: string
+  private: boolean
+  owner: Owner
+  html_url: string
+  description?: any
+  fork: boolean
+  url: string
+  forks_url: string
+  keys_url: string
+  collaborators_url: string
+  teams_url: string
+  hooks_url: string
+  issue_events_url: string
+  events_url: string
+  assignees_url: string
+  branches_url: string
+  tags_url: string
+  blobs_url: string
+  git_tags_url: string
+  git_refs_url: string
+  trees_url: string
+  statuses_url: string
+  languages_url: string
+  stargazers_url: string
+  contributors_url: string
+  subscribers_url: string
+  subscription_url: string
+  commits_url: string
+  git_commits_url: string
+  comments_url: string
+  issue_comment_url: string
+  contents_url: string
+  compare_url: string
+  merges_url: string
+  archive_url: string
+  downloads_url: string
+  issues_url: string
+  pulls_url: string
+  milestones_url: string
+  notifications_url: string
+  labels_url: string
+  releases_url: string
+  deployments_url: string
+  created_at: Date
+  updated_at: Date
+  pushed_at: Date
+  git_url: string
+  ssh_url: string
+  clone_url: string
+  svn_url: string
+  homepage?: any
+  size: number
+  stargazers_count: number
+  watchers_count: number
+  language: string
+  has_issues: boolean
+  has_projects: boolean
+  has_downloads: boolean
+  has_wiki: boolean
+  has_pages: boolean
+  forks_count: number
+  mirror_url?: any
+  archived: boolean
+  disabled: boolean
+  open_issues_count: number
+  license?: any
+  forks: number
+  open_issues: number
+  watchers: number
+  default_branch: string
 }
 
 export interface Sender {
-  login: string;
-  id: number;
-  node_id: string;
-  avatar_url: string;
-  gravatar_id: string;
-  url: string;
-  html_url: string;
-  followers_url: string;
-  following_url: string;
-  gists_url: string;
-  starred_url: string;
-  subscriptions_url: string;
-  organizations_url: string;
-  repos_url: string;
-  events_url: string;
-  received_events_url: string;
-  type: string;
-  site_admin: boolean;
+  login: string
+  id: number
+  node_id: string
+  avatar_url: string
+  gravatar_id: string
+  url: string
+  html_url: string
+  followers_url: string
+  following_url: string
+  gists_url: string
+  starred_url: string
+  subscriptions_url: string
+  organizations_url: string
+  repos_url: string
+  events_url: string
+  received_events_url: string
+  type: string
+  site_admin: boolean
 }
 
 export interface ReviewPayload {
-  action: string;
-  review: Review;
-  pull_request: PullRequest;
-  repository: Repository;
-  sender: Sender;
+  action: string
+  review: Review
+  pull_request: PullRequest
+  repository: Repository
+  sender: Sender
 }
