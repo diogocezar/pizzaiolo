1bdd9f857b4d09cdad1952ee8f83a323316f5ebb
Jott4 <jvgcunha2002@gmail.com>
2022-10-26 17:14:25 -0300

    refactor: refatorado tipagem para simplificar o payload

---
+++ src/types/base/links.d.ts
@@ -0,0 +1,10 @@
+export interface Links {
+  self: Self
+  html: Html
+  issue: Issue
+  comments: Comments
+  review_comments: ReviewComments
+  review_comment: ReviewComment
+  commits: Commits
+  statuses: Statuses
+}
