692be8db030fdb03918e0cff88c064bac92772a0
joao208 <augustoj311@gmail.com>
2022-10-30 17:18:06 -0300

    feat: refact guards

--- src/common/guards/github.guard.ts
+++ src/common/guards/github.guard.ts
@@ -1,26 +1,18 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common'
 import * as crypto from 'crypto'
-import * as rawBody from 'raw-body'
 
 @Injectable()
 export class GithubWebHookGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.switchToHttp().getRequest()
-    if (req.readable) {
-      const X_HUB_SIGNATURE = 'x-hub-signature'
-      const bodyFormatted = await rawBody(req)
-      const chunk = bodyFormatted.toString().trim()
-      const headerSignature = req.headers[X_HUB_SIGNATURE]
-      // TODO: Temporary to validate!
-      console.log(headerSignature)
-      const signature = `sha1=${crypto
-        .createHmac('sha1', process.env.GITHUB_WEBHOOK_SECRET)
-        .update(chunk)
-        .digest('hex')}
-      `
-      console.log(signature)
-      return headerSignature === signature
-    }
-    return false
+
+    const algo = crypto
+      .createHmac('sha1', process.env.GITHUB_WEBHOOK_SECRET)
+      .update(req.rawBody)
+      .digest('hex')
+
+    const signature = `sha1=${algo}`
+
+    return signature === req.headers['x-hub-signature']
   }
 }
