ad7b7a1df8181d9a8d4ba37fd0574ef1329a1a32
joao208 <augustoj311@gmail.com>
2022-10-30 17:19:46 -0300

    fix: fixing constant name

--- src/common/guards/github.guard.ts
+++ src/common/guards/github.guard.ts
@@ -6,12 +6,12 @@ export class GithubWebHookGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.switchToHttp().getRequest()
 
-    const algo = crypto
+    const encryptedBody = crypto
       .createHmac('sha1', process.env.GITHUB_WEBHOOK_SECRET)
       .update(req.rawBody)
       .digest('hex')
 
-    const signature = `sha1=${algo}`
+    const signature = `sha1=${encryptedBody}`
 
     return signature === req.headers['x-hub-signature']
   }
