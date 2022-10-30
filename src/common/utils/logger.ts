94ccede6b4580b3a8b9e56b6571cbce1ff86b0af
joao208 <augustoj311@gmail.com>
2022-10-30 11:52:27 -0300

    feat: remove transporter

--- src/common/utils/logger.ts
+++ src/common/utils/logger.ts
@@ -6,10 +6,6 @@ const Logger = () => {
       level: 'info',
       format: winston.format.json(),
       defaultMeta: { service: 'user-service' },
-      transports: [
-        new winston.transports.File({ filename: 'error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'combined.log' }),
-      ],
     })
     .add(
       new winston.transports.Console({
