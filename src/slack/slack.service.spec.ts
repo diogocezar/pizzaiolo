86c7dc41342a7c316acb7118406f2192019f5bd1
Jott4 <jvgcunha2002@gmail.com>
2022-10-30 00:09:40 -0300

    test: add slack.service tests

--- src/slack/slack.service.spec.ts
+++ src/slack/slack.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { SlackApi } from 'src/config/api/slack.api'
 import { SlackService } from './slack.service'
 
 describe('SlackService', () => {
@@ -6,7 +7,21 @@ describe('SlackService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SlackService],
+      providers: [
+        SlackService,
+        {
+          provide: SlackApi,
+          useValue: {
+            post: jest.fn(() =>
+              Promise.resolve({
+                data: {
+                  ok: true,
+                },
+              })
+            ),
+          },
+        },
+      ],
     }).compile()
 
     service = module.get<SlackService>(SlackService)
@@ -14,5 +29,23 @@ describe('SlackService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  it('should send a message to slack', async () => {
+    const message = {
+      text: 'Hello World',
+    }
+    const response = await service.sendMessage(message)
+
+    expect(response).toEqual({ ok: true })
+  })
+
+  it('should add a reaction to a message', async () => {
+    const reaction = {
+      name: 'thumbsup',
+    }
+    const response = await service.addReaction(reaction)
+
+    expect(response).toBeUndefined()
   })
 })
