b0d7f4cdf55706726aa4739cbb5812924fefb51e
joao208 <augustoj311@gmail.com>
2022-11-02 14:40:11 -0300

    feat: change text to more responsivity

--- src/common/constants/messages.constants.ts
+++ src/common/constants/messages.constants.ts
@@ -1,6 +1,6 @@
 const MESSAGES = {
   OPEN_PULL_REQUEST: '🤌 Mama Mia! Que código bonito! 🤌\n',
-  COMMENT: '🍕 Mama Mia! *Reclamação do Cliente!* 🍕',
+  COMMENT: '🍕 Mama Mia! *Reclamação do Cliente!*',
   RESOLVED: '✅ Uma thread foi resolvida!\n',
   UNRESOLVED: '❌ Uma thread não foi resolvida!\n',
   OPENED: '👉 Uma thread foi marcada como aberta!\n',
