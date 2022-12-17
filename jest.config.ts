9962cb3d3b9b25760e29ef4c5ada786f6dfc6bfa
Diogo Cezar <8019111+diogocezar@users.noreply.github.com>
2022-12-17 17:31:30 -0300

    feat: includding sonar

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+import { pathsToModuleNameMapper } from 'ts-jest'
+import { compilerOptions } from './tsconfig.json'
+import { Config } from 'jest'
+
+const config: Config = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: './',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+}
+
+export default config
